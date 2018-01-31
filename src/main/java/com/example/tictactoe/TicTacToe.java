8f6bb74d0814e9886223ebff1ea739c12ce8ddb6
lpelczar <lukepelczar@gmail.com>
2018-01-31 19:19:28 +0100

    Refactor is winner method

--- src/main/java/com/example/tictactoe/TicTacToe.java
+++ src/main/java/com/example/tictactoe/TicTacToe.java
@@ -22,9 +22,8 @@ public class TicTacToe {
     private boolean isWinner() {
         int playerTotal = lastPlayer * SIZE;
         for (int i = 0; i < SIZE; i++)
-            if (board[0][i] + board[1][i] + board[2][i] == playerTotal) {
-                return true;
-            } else if (board[i][0] + board[i][1] + board[i][2] == playerTotal) {
+            if (board[0][i] + board[1][i] + board[2][i] == playerTotal ||
+                board[i][0] + board[i][1] + board[i][2] == playerTotal) {
                 return true;
             }
         return false;
