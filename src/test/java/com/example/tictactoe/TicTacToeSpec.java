f3f382013e4ba96314328ac91f16241504f179ba
lpelczar <lukepelczar@gmail.com>
2018-01-31 18:41:00 +0100

    Add test checking if there is no winner

--- src/test/java/com/example/tictactoe/TicTacToeSpec.java
+++ src/test/java/com/example/tictactoe/TicTacToeSpec.java
@@ -46,4 +46,10 @@ public class TicTacToeSpec {
         ticTacToe.play(1,1);
         assertEquals('O', ticTacToe.nextPlayer());
     }
+
+    @Test
+    public void whenPlayThenNoWinner() {
+        String actual = ticTacToe.play(1,1);
+        assertEquals("No winner", actual);
+    }
 }
