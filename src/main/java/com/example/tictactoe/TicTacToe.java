4f649a50d23f8c2add6a0756d7675fee36868da7
lpelczar <lukepelczar@gmail.com>
2018-01-31 15:13:10 +0100

    Add code passing the test with occupied field

--- src/main/java/com/example/tictactoe/TicTacToe.java
+++ src/main/java/com/example/tictactoe/TicTacToe.java
@@ -2,12 +2,21 @@ package com.example.tictactoe;
 
 public class TicTacToe {
 
+    private Character[][] board = {{'\0', '\0', '\0'},
+                                   {'\0', '\0', '\0'},
+                                   {'\0', '\0', '\0'}};
+
     public void play(int column, int row) {
 
         if (column < 1 || column > 3) {
             throw new RuntimeException("X value is outside the board!");
         } else if (row < 1 || row > 3) {
             throw new RuntimeException("Y value is outside the board!");
+        }
+        if (board[column - 1][row - 1] != '\0') {
+            throw new RuntimeException("Field is occupied!");
+        } else {
+            board[column - 1][row - 1] = 'X';
         }
     }
 }
