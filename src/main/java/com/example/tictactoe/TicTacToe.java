e25be1c41b4c4524e7145b0abc3ef6245192ecd5
lpelczar <lukepelczar@gmail.com>
2018-01-31 14:37:26 +0100

    Add passing test when x outside the board

--- src/main/java/com/example/tictactoe/TicTacToe.java
+++ src/main/java/com/example/tictactoe/TicTacToe.java
@@ -1,4 +1,11 @@
 package com.example.tictactoe;
 
 public class TicTacToe {
+
+    public void play(int column, int row) {
+
+        if (column < 1 || column > 3) {
+            throw new RuntimeException("X value is outside the board!");
+        }
+    }
 }
