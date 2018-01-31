3b00e1ec59f02d2381e98270f0e10263601a0423
lpelczar <lukepelczar@gmail.com>
2018-01-31 14:25:49 +0100

    Add test checking exception when x is outside border

--- src/test/java/com/example/tictactoe/TicTacToeSpec.java
+++ src/test/java/com/example/tictactoe/TicTacToeSpec.java
@@ -1,4 +1,24 @@
 package com.example.tictactoe;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class TicTacToeSpec {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private TicTacToe ticTacToe;
+
+    @Before
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
 }
