b55a92313761b27407fab77fb868a6c8f7b2fe40
lpelczar <lukepelczar@gmail.com>
2018-01-31 19:18:20 +0100

    Add code checking vertical line winner

--- src/main/java/com/example/tictactoe/TicTacToe.java
+++ src/main/java/com/example/tictactoe/TicTacToe.java
@@ -20,8 +20,11 @@ public class TicTacToe {
     }
 
     private boolean isWinner() {
+        int playerTotal = lastPlayer * SIZE;
         for (int i = 0; i < SIZE; i++)
-            if (board[0][i] + board[1][i] + board[2][i] == (lastPlayer * SIZE)) {
+            if (board[0][i] + board[1][i] + board[2][i] == playerTotal) {
+                return true;
+            } else if (board[i][0] + board[i][1] + board[i][2] == playerTotal) {
                 return true;
             }
         return false;
