b5674322d84f0286c40af0c5b27d60cd71e78e9c
lpelczar <lukepelczar@gmail.com>
2018-01-31 19:13:59 +0100

    Add test checking vertical line winner

--- src/test/java/com/example/tictactoe/TicTacToeSpec.java
+++ src/test/java/com/example/tictactoe/TicTacToeSpec.java
@@ -62,4 +62,15 @@ public class TicTacToeSpec {
         String actual = ticTacToe.play(3,1); // X
         assertEquals("X is the Winner", actual);
     }
+
+    @Test
+    public void whenPlayAndWholeVerticalLineThenWinner() {
+        ticTacToe.play(2,1); // X
+        ticTacToe.play(1,1); // O
+        ticTacToe.play(3,1); // X
+        ticTacToe.play(1,2); // O
+        ticTacToe.play(2,2); // X
+        String actual = ticTacToe.play(1,3); // O
+        assertEquals("O is the Winner", actual);
+    }
 }
