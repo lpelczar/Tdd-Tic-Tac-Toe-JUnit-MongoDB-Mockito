88d09d28cbb2614201cc8919ec049efcd920e40b
lpelczar <lukepelczar@gmail.com>
2018-02-21 17:26:11 +0100

    Refactor test methods

--- src/test/java/com/example/tictactoe/mongo/TicTacToeCollectionSpec.java
+++ src/test/java/com/example/tictactoe/mongo/TicTacToeCollectionSpec.java
@@ -1,5 +1,6 @@
 package com.example.tictactoe.mongo;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.net.UnknownHostException;
@@ -8,15 +9,20 @@ import static org.junit.Assert.assertEquals;
 
 public class TicTacToeCollectionSpec {
 
+    private TicTacToeCollection collection;
+
+    @Before
+    public void before() throws UnknownHostException {
+        collection = new TicTacToeCollection();
+    }
+
     @Test
-    public void whenInstantiatedThenMongoDbNameIsTicTacToe() throws UnknownHostException {
-        TicTacToeCollection collection = new TicTacToeCollection();
+    public void whenInstantiatedThenMongoDbNameIsTicTacToe() {
         assertEquals("tic-tac-toe", collection.getMongoCollection().getDBCollection().getDB().getName());
     }
 
     @Test
-    public void whenInstantiatedThenMongoCollectionHasNameGame() throws UnknownHostException {
-        TicTacToeCollection collection = new TicTacToeCollection();
+    public void whenInstantiatedThenMongoCollectionHasNameGame() {
         assertEquals("game", collection.getMongoCollection().getName());
     }
 }
