1aedb7cc67b0302a967bb83b8ee88bb1014944f0
lpelczar <lukepelczar@gmail.com>
2018-02-21 17:15:55 +0100

    Add constructor to tic tac toe collection class

--- src/main/java/com/example/tictactoe/TicTacToeCollection.java
+++ src/main/java/com/example/tictactoe/TicTacToeCollection.java
@@ -1,4 +1,22 @@
 package com.example.tictactoe;
 
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+
+import java.net.UnknownHostException;
+
 public class TicTacToeCollection {
+
+    private MongoCollection mongoCollection;
+
+    protected MongoCollection getMongoCollection() {
+        return mongoCollection;
+    }
+
+    public TicTacToeCollection() throws UnknownHostException {
+        DB db = new MongoClient().getDB("tic-tac-toe");
+        mongoCollection = new Jongo(db).getCollection("foo");
+    }
 }
