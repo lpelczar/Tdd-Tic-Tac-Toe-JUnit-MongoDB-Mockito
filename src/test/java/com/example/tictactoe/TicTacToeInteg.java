7222305d0c1d9c2cb1fc24a28b47e7e41455f52e
lpelczar <lukepelczar@gmail.com>
2018-02-23 14:21:14 +0100

    Remove sample integration test

--- src/test/java/com/example/tictactoe/TicTacToeInteg.java
+++ src/test/java/com/example/tictactoe/TicTacToeInteg.java
@@ -1,16 +1,3 @@
 package com.example.tictactoe;
 
-import org.junit.Test;
-
-import java.net.UnknownHostException;
-
-import static org.junit.Assert.assertEquals;
-
-public class TicTacToeInteg {
-
-    @Test
-    public void givenMongoDbIsRunningWhenPlayThenNoException() throws UnknownHostException {
-        TicTacToe ticTacToe = new TicTacToe();
-        assertEquals("No winner", ticTacToe.play(1, 1));
-    }
-}
+public class TicTacToeInteg {}
